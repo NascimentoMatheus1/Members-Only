e086b9eeb538219fadb6a73a46f58326a12d9954
Matheus Nascimento <matheus_nascimento13@hotmail.com>
2026-02-16 20:53:39 -0300

    Feat(views): Configurar EJS como view engine

--- src/app.js
+++ src/app.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const router = require('./routes/basicRouter');
+const path = require('path');
 const app = express();
 
+// VIEW ENGINE CONFIG
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+
+// ROUTES
 app.use('/', router);
 
 module.exports = app;
--- src/routes/basicRouter.js
+++ src/routes/basicRouter.js
@@ -3,7 +3,7 @@ const { Router } = require('express');
 const router = Router();
 
 router.get('/', (req, res) => {
-    res.send('hello world!');
+    res.render('index');
 });
 
 module.exports = router;
