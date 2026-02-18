7abb9f0e811d19316e65ea8c99c050e31d7f4d9a
Matheus Nascimento <matheus_nascimento13@hotmail.com>
2026-02-17 22:08:09 -0300

    Feat(db): Implementado SQL queries de procurar usuario por username e adicionar usuario

---
+++ src/db/queries.js
@@ -0,0 +1,27 @@
+const pool = require('./pool');
+
+async function getByUsername(username) {
+    const { rows } = await pool.query(
+        'SELECT * FROM users WHERE username = $1',
+        [username],
+    );
+
+    return rows[0];
+}
+
+async function createUser(firstname, lastname, username, password_hash) {
+    const { rows } = await pool.query(
+        `
+        INSERT INTO users (first_name, last_name, username, password)
+        VALUES ($1, $2, $3, $4) RETURNING *
+        `,
+        [firstname, lastname, username, password_hash],
+    );
+
+    return rows[0];
+}
+
+module.exports = {
+    getByUsername,
+    createUser,
+};
