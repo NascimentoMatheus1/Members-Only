786c6ce16b28b947f38455e9d63691b82d3ebc1b
Matheus Nascimento <matheus_nascimento13@hotmail.com>
2026-02-19 17:43:58 -0300

    Feat: Adicionar a configuração do express-session

--- src/app.js
+++ src/app.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const expressSession = require('express-session');
+const pgPool = require('../src/db/pool');
 const router = require('./routes/basicRouter');
 const path = require('path');
 const app = express();
@@ -7,10 +9,26 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// MIDDLEWARE Static files folder
+// Static files folder - Middleware
 app.use(express.static(path.join(__dirname, 'public')));
-// FORM
+
+// Form Parsing - Middleware
 app.use(express.urlencoded({ extended: true }));
+
+// Session - Middleware
+const pgSession = require('connect-pg-simple')(expressSession);
+app.use(
+    expressSession({
+        store: new pgSession({
+            pool: pgPool,
+            tableName: 'session',
+        }),
+        secret: process.env.FOO_COOKIE_SECRET,
+        resave: false,
+        saveUninitialized: false,
+        cookie: { secure: true, maxAge: 7 * 24 * 60 * 60 * 1000 },
+    }),
+);
 
 // ROUTES
 app.use('/', router);
