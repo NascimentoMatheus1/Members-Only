3592a508969e5da40ada0b4d6ad71b9c95c09b18
Matheus Nascimento <matheus_nascimento13@hotmail.com>
2026-02-19 13:04:00 -0300

    Feat(db): Implementar a query SQL que atualiza o status para membro do usuario

--- src/db/queries.js
+++ src/db/queries.js
@@ -21,7 +21,17 @@ async function createUser(firstname, lastname, username, password_hash) {
     return rows[0];
 }
 
+async function updateUserBecomeMember(id) {
+    const { rows } = await pool.query(
+        `
+        UPDATE users SET membership_status = true WHERE id = $1 RETURNING *;
+        `,
+    );
+    return rows[0];
+}
+
 module.exports = {
     getByUsername,
     createUser,
+    updateUserBecomeMember,
 };
