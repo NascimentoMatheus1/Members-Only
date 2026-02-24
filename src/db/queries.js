f58de0cb0a988d59c67d26cde57f04b047cd6dce
Matheus Nascimento <matheus_nascimento13@hotmail.com>
2026-02-24 18:50:54 -0300

    Fix(db): Adicionar bloco try/catch nas queries para impedir o crash do servidor

--- src/db/queries.js
+++ src/db/queries.js
@@ -1,33 +1,45 @@
 const pool = require('./pool');
 
 async function getByUsername(username) {
-    const { rows } = await pool.query(
-        'SELECT * FROM users WHERE username = $1',
-        [username],
-    );
+    try {
+        const { rows } = await pool.query(
+            'SELECT * FROM users WHERE username = $1',
+            [username],
+        );
 
-    return rows[0];
+        return rows[0];
+    } catch (error) {
+        console.log(error.message);
+    }
 }
 
 async function createUser(firstname, lastname, username, password_hash) {
-    const { rows } = await pool.query(
-        `
+    try {
+        const { rows } = await pool.query(
+            `
         INSERT INTO users (first_name, last_name, username, password)
         VALUES ($1, $2, $3, $4) RETURNING *
         `,
-        [firstname, lastname, username, password_hash],
-    );
+            [firstname, lastname, username, password_hash],
+        );
 
-    return rows[0];
+        return rows[0];
+    } catch (error) {
+        console.log(error.message);
+    }
 }
 
 async function updateUserBecomeMember(id) {
-    const { rows } = await pool.query(
-        `
+    try {
+        const { rows } = await pool.query(
+            `
         UPDATE users SET membership_status = true WHERE id = $1 RETURNING *;
         `,
-    );
-    return rows[0];
+        );
+        return rows[0];
+    } catch (error) {
+        console.log(error.message);
+    }
 }
 
 module.exports = {
