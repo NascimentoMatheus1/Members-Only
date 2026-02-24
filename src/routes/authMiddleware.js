218deed51357d95eaec16f98e429dcb96cc1783a
Matheus Nascimento <matheus_nascimento13@hotmail.com>
2026-02-24 18:42:09 -0300

    Feat(ui): Criar pagina de error para as credenciais de autenticação são inválidas

--- src/routes/authMiddleware.js
+++ src/routes/authMiddleware.js
@@ -2,8 +2,8 @@ module.exports.isAuth = (req, res, next) => {
     if (req.isAuthenticated()) {
         next();
     } else {
-        res.status(401).json({
-            msg: 'User not authorized to view this resource',
+        res.status(401).render('pages/not_authorized_page', {
+            title: 'Not Authorized',
         });
     }
 };
