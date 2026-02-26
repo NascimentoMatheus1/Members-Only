96bbbfb5a7971da54941dfae3a1c9ed77d4be6a2
Matheus Nascimento <matheus_nascimento13@hotmail.com>
2026-02-26 19:43:48 -0300

    Fix: Corrigir a lógica que verificar se o usuario é um membro
    - Apenas membros podem ver as informações das mensagens

--- src/controllers/indexControler.js
+++ src/controllers/indexControler.js
@@ -1,20 +1,33 @@
 const messagesDB = require('../db/messageQueries');
 
 const getIndexPage = async (req, res) => {
-    if (req.isAuthenticated()) {
-        const { username, membership_status } = req.user;
-        const messages = await messagesDB.selectAllMessagesInnerJoinUser();
+    try {
+        const currentUser = req.user
+            ? {
+                  username: req.user.username,
+                  membership_status: req.user.membership_status,
+              }
+            : null;
+
+        if (req.isAuthenticated() && req.user.membership_status) {
+            const messages = await messagesDB.selectAllMessagesInnerJoinUser();
+            return res.render('pages/index', {
+                title: 'Home Member',
+                currentUser,
+                posts: messages,
+            });
+        }
+
+        const messages = await messagesDB.selectAllMessages();
+        
         return res.render('pages/index', {
-            title: 'Home Member',
-            currentUser: { username, membership_status },
+            title: 'Home',
+            currentUser,
             posts: messages,
         });
+    } catch (error) {
+        console.log(error.message);
     }
-    const messages = await messagesDB.selectAllMessages();
-    return res.render('pages/index', {
-        title: 'Home',
-        posts: messages,
-    });
 };
 
 module.exports = { getIndexPage };
