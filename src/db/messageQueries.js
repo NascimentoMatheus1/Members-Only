4c3eaaa3bda4743b8ec75ad351dc5b223ed45082
Matheus Nascimento <matheus_nascimento13@hotmail.com>
2026-02-26 18:32:56 -0300

    Feat(db): Implementar SQL queries da tabela de mensagens
    - selecionar todas as mensagens
    
    - selecionar todas as mensagens com informações do usuario
    
    - inserir nova mensagem

---
+++ src/db/messageQueries.js
@@ -0,0 +1,43 @@
+const pool = require('./pool');
+
+async function selectAllMessages() {
+    try {
+        const { rows } = await pool.query('SELECT * FROM messages;');
+        return rows;
+    } catch (error) {
+        console.log(error.messages);
+    }
+}
+
+async function selectAllMessagesInnerJoinUser() {
+    try {
+        const { rows } = await pool.query(`
+            SELECT title, content, messages.create_at, 
+            first_name, last_name, username FROM messages 
+            INNER JOIN users 
+            ON (messages.user_id = users.id);`);
+        return rows;
+    } catch (error) {
+        console.log(error.messages);
+    }
+}
+
+async function insertNewMessage(title, message, userId) {
+    try {
+        const { rows } = await pool.query(
+            `
+        INSERT INTO messages (title, content, user_id) 
+        VALUES ($1, $2, $3) RETURNING *`,
+            [title, message, userId],
+        );
+        return rows[0];
+    } catch (error) {
+        console.log(error.messages);
+    }
+}
+
+module.exports = {
+    selectAllMessages,
+    selectAllMessagesInnerJoinUser,
+    insertNewMessage,
+};
