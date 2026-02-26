e468bc6eb6c3c8b7f17b6e87b40a8a13e9a465ec
Matheus Nascimento <matheus_nascimento13@hotmail.com>
2026-02-26 16:18:14 -0300

    Refactor: Implementar pagina de error para diferentes codigos de status

--- src/routes/authMiddleware.js
+++ src/routes/authMiddleware.js
@@ -2,8 +2,24 @@ module.exports.isAuth = (req, res, next) => {
     if (req.isAuthenticated()) {
         next();
     } else {
-        res.status(401).render('pages/not_authorized_page', {
+        res.status(401).render('pages/error_page', {
             title: 'Not Authorized',
+            status: 401,
+            message: 'Access Denied / Not Authorized ',
+            notice: `Sorry, you don't have permission to access this page. Please check your credentials.`,
+        });
+    }
+};
+
+module.exports.isMember = (req, res, next) => {
+    if (req.isAuthenticated() && req.user.membership_status) {
+        next();
+    } else {
+        res.status(403).render('pages/error_page', {
+            title: 'Forbidden',
+            status: 403,
+            message: `Forbidden request – You don't have permission to access`,
+            notice: `That's an error. We're sorry, but you do not have access to this page. `,
         });
     }
 };
