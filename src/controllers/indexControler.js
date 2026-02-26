9e4f413ea1fed586ea7d08e46d27e08f9ef83d2d
Matheus Nascimento <matheus_nascimento13@hotmail.com>
2026-02-26 18:22:18 -0300

    Feat: Implementar a lógica que mostra os dados das postagens com detalhes diferentes para cada usuario

--- src/controllers/indexControler.js
+++ src/controllers/indexControler.js
@@ -1,15 +1,19 @@
 const messagesDB = require('../db/messageQueries');
 
-const getIndexPage = (req, res) => {
+const getIndexPage = async (req, res) => {
     if (req.isAuthenticated()) {
         const { username, membership_status } = req.user;
+        const messages = await messagesDB.selectAllMessagesInnerJoinUser();
         return res.render('pages/index', {
             title: 'Home Member',
             currentUser: { username, membership_status },
+            posts: messages,
         });
     }
+    const messages = await messagesDB.selectAllMessages();
     return res.render('pages/index', {
         title: 'Home',
+        posts: messages,
     });
 };
 
