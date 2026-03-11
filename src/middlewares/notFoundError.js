3d7b12f34cd7b2b9dc7501043f0b51186c0f029b
Matheus Nascimento <matheus_nascimento13@hotmail.com>
2026-03-11 17:14:32 -0300

    Feat: Adicionar erros versão pt-br para os middlewares

--- src/middlewares/notFoundError.js
+++ src/middlewares/notFoundError.js
@@ -1,8 +1,8 @@
 module.exports.getNotFoundPage = (req, res) => {
     res.render('pages/error_page.ejs', {
-        title: 'Not Found',
-        status: '404 Error',
-        message: 'Not Found',
-        notice: "The page you're looking for can't be found",
+        title: 'Página não encontrada',
+        status: 'Erro 404',
+        message: 'Página não encontrada',
+        notice: "Desculpe, mas o conteúdo que você busca não existe ou foi movido.",
     });
 };
